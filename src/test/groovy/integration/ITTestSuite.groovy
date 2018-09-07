001e287510bdb4f1253a9928ef1da0b0f7f44400
Robert Hicks <roberthicksiii@gmail.com>
2018-09-07 16:29:03 -0400

    Adding import statement to test file

--- src/test/groovy/integration/ITTestSuite.groovy
+++ src/test/groovy/integration/ITTestSuite.groovy
@@ -3,6 +3,7 @@ package integration
 import integration.test.ZOwners
 import integration.test.ZSupplements
 import integration.test.ZMain
+import integration.test.ZOwnersList
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
